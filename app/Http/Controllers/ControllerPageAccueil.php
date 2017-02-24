9d111a28b1abddd4b1ed12fe1f40cab4e12d99e4
Ru Tan <jutan2012@gmail.com>
2017-02-24 15:18:04 +0100

    Afficher rendom sur les images

--- app/Http/Controllers/ControllerPageAccueil.php
+++ app/Http/Controllers/ControllerPageAccueil.php
@@ -4,15 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Images;
-
 class ControllerPageAccueil extends Controller
 {
-	public function getIndex(){
-		$image = Images::all();
-		return view('PageAccueil', ['image'=>$image]);
+	public function getRandom(){
+		$root = 'mesImages';
+		$images = glob($root.'/*.jpeg', GLOB_BRACE); 
+		$randomImage = $images[array_rand($images)];
+		return view('PageAccueil', ['image'=>$randomImage]);		
 	}
-	public function getShow(){
-		$image = Images::all();
-		return view ('PageAccueil');
-	}
+	
 }
